f9e854d53a5c2666bac77311045832d5bdd0b6d4
Andres <afcastel@bancolombia.com.co>
2022-10-20 11:32:02 -0500

    first commit test

--- src/test/java/retoAutomatizacion/stepdefinitions/StepDefinitionsRegister.java
+++ src/test/java/retoAutomatizacion/stepdefinitions/StepDefinitionsRegister.java
@@ -4,7 +4,6 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import retoAutomatizacion.tasks.OpenPage;
